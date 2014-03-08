705e38d64a7fd2c6d79defde3e497321fa1d3658
Cheng Liu <cheng.liu@geneegroup.com>
2014-03-08 09:34:44 +0800

    fix none-cache get/set function

--- class/gini/cache/none.php
+++ class/gini/cache/none.php
@@ -7,9 +7,9 @@ class None implements \Gini\Cache\Driver
     public function __construct($name, array $options)
     {}
 
-    public function set($key, $value) { }
+    public function set($key, $value, $ttl) { }
 
-    public function get($key) { }
+    public function get($key) { return false; }
 
     public function remove($key) { }
 
